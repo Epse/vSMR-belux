e8bed835cc1492125808fb9e87ea507ea263a216
Stef Pletinck <s@stefpletinck.be>
2024-05-20 10:02:38 +0200

    Implement gate target labels
    the graphics code is as usual slightly blursed,
    but oh well it's graphics

--- vSMR/GateTarget.cpp
+++ vSMR/GateTarget.cpp
@@ -116,7 +116,7 @@ unsigned char GateTarget::calculateTargetSize(EuroScopePlugIn::CRadarScreen* rad
 	);
 }
 
-void GateTarget::OnRefresh(CSMRRadar* radar_screen, Gdiplus::Graphics* graphics)
+void GateTarget::OnRefresh(CSMRRadar* radar_screen, Gdiplus::Graphics* graphics, Gdiplus::Font* font)
 {
 	const EuroScopePlugIn::CFlightPlan fp = radar_screen->GetPlugIn()->FlightPlanSelectASEL();
 	const std::string airport = radar_screen->getActiveAirport();
@@ -136,17 +136,40 @@ void GateTarget::OnRefresh(CSMRRadar* radar_screen, Gdiplus::Graphics* graphics)
 		return;
 	const POINT pos = radar_screen->ConvertCoordFromPositionToPixel(maybe_pos.value());
 
-	const auto brush = Gdiplus::SolidBrush(Gdiplus::Color(255, 0, 160, 250));
+	const auto bright_blue = Gdiplus::Color(255, 0, 160, 250);
+	const auto brush = Gdiplus::SolidBrush(bright_blue);
 	const auto pen = Gdiplus::Pen(Gdiplus::Color::Black, 2);
 
 	const auto size_px = GateTarget::calculateTargetSize(radar_screen);
 
 	const double angle = calculateAsrAngle(radar_screen);
+	const auto indicator_angle = (gate_info.heading  + angle);
 	graphics->TranslateTransform(pos.x, pos.y);
-	graphics->RotateTransform(gate_info.heading - 180 + angle);
+	graphics->RotateTransform(indicator_angle - 180);
 	Gdiplus::Point points[GateTarget::POINTS_IN_INDICATOR];
 	this->getIndicator(points, POINT{0, 0}, size_px);
 	graphics->FillPolygon(&brush, points, GateTarget::POINTS_IN_INDICATOR);
 	graphics->DrawPolygon(&pen, points, GateTarget::POINTS_IN_INDICATOR);
+	graphics->ResetTransform();
+
+	graphics->TranslateTransform(pos.x, pos.y);
+	const auto line_pen = Gdiplus::Pen(bright_blue, 2);
+	const auto clamped_angle = (static_cast<int>(indicator_angle)) % 360;
+	const auto sign = clamped_angle >= 90 && clamped_angle <= 270 ? +1 : -1;
+	graphics->DrawLine(&line_pen, 0, 0, 0, sign * 30);
+
+	const wstring wgate = wstring(gate_info.gate.begin(), gate_info.gate.end());
+	auto label_text_rect = Gdiplus::RectF();
+	graphics->MeasureString(wgate.c_str(), wgate.length(), font, PointF(GateTarget::LABEL_PADDING, GateTarget::LABEL_PADDING + sign * 30), &label_text_rect);
+	// Look, I don't know why putting 2*padding on the Y makes it too high and also not center the text, but here we are I guess
+	const auto label_rect = Gdiplus::RectF(0, sign * 30, label_text_rect.Width + 2 * GateTarget::LABEL_PADDING, label_text_rect.Height + GateTarget::LABEL_PADDING);
+
+	const auto label_bg_brush = Gdiplus::SolidBrush(Gdiplus::Color(100, 0, 70, 195)); // The alpha is a guess
+	const auto label_border_pen = Gdiplus::Pen(bright_blue);
+	const auto text_brush = Gdiplus::SolidBrush(Gdiplus::Color::White);
+	graphics->FillRectangle(&label_bg_brush, label_rect);
+	graphics->DrawRectangle(&label_border_pen, label_rect);
+	graphics->DrawString(wgate.c_str(), wgate.length(), font, Gdiplus::PointF(GateTarget::LABEL_PADDING, GateTarget::LABEL_PADDING + sign * 30), &text_brush);
+
 	graphics->ResetTransform();
 }
